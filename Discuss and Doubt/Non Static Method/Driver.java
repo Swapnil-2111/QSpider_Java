eadb6ce8889995032e5cbd8c927b479c25666e2c
Swapnil-2111 <swapnilmete2002@gmail.com>
2025-09-19 14:56:28 +0530

    minor changes in java

--- Discuss and Doubt/Non Static Method/Driver.java
+++ Discuss and Doubt/Non Static Method/Driver.java
@@ -1,25 +1,28 @@
 // package Discuss and Doubt.Non Static Method;
-class Parent{
-    public void m1(){
+class Parent {
+    public void m1() {
         System.out.println("Parent m1()");
     }
-    public void m2(){
+
+    public void m2() {
         System.out.println("m2() parent");
     }
 }
 
-class Child extends Parent{
+class Child extends Parent {
     @Override
-    public void m1(){
+    public void m1() {
         System.out.println("m1() child");
     }
-    public void m3(){
-        System.out.println("m3() child");
+
+    public void m3() {
+        System.out.println("m3() ");
     }
 }
+
 public class Driver {
     public static void main(String[] args) {
-        Child obj=new Child();
+        Child obj = new Child();
         obj.m1();
         obj.m2();
         obj.m3();
